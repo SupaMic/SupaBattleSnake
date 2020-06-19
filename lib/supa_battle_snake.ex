7e5664216fea9dc37fba025acb41cb1d1a330462
Supamic <970239+SupaMic@users.noreply.github.com>
2020-06-19 22:48:35 +0000

    numerous changes(strategy optimal move needs fixing), preparing for deployment scripts

--- lib/supa_battle_snake.ex
+++ lib/supa_battle_snake.ex
@@ -1,5 +1,5 @@
 defmodule SupaBattleSnake.GameBoard do
-  defstruct game_id: "", timeout: 500, turn: 0, board_food: [], board_height: 0, you_id: "", snakes: [], route_state: nil, up: 1.0, right: 1.0, down: 1.0, left: 1.0
+  defstruct game_id: "", timeout: 500, turn: 0, board_food: [], board_height: 0, you_id: "", snakes: [], route_state: nil, up: 0.5, right: 0.5, down: 0.5, left: 0.5
 end
 
 
@@ -36,18 +36,17 @@ defmodule SupaBattleSnake do
       #IO.inspect(game_params, label: game_state)
       
       list_of_snakes = convert_to_list_of_snakes(snakes_list_of_maps, you_id)
-      list_of_food = convert_to_coords(food_list_of_maps)
+      #list_of_food = convert_to_coords(food_list_of_maps)
       
       game_converted = %GameBoard{game_id: game_id, timeout: timeout_int, board_height: height_int, turn: turn_int, 
-                                  board_food: list_of_food, you_id: you_id, snakes: list_of_snakes, route_state: game_state,
-                                  up: 1.0, right: 1.0, down: 1.0, left: 1.0
+                                  board_food: food_list_of_maps, you_id: you_id, snakes: list_of_snakes, route_state: game_state,
+                                  up: 0.5, right: 0.5, down: 0.5, left: 0.5
                                   }
      
       #IO.inspect(game_converted, label: game_state)
   end
   
   def convert_to_list_of_snakes(snakes_list_of_maps, you_id) do
-    
     snakes_list_of_maps
     |> Enum.map(fn map -> %Snake{body: Map.get(map, "body"), 
                            head: Map.get(map, "head"),
@@ -58,15 +57,6 @@ defmodule SupaBattleSnake do
                            shout: Map.get(map, "shout"),
                            you: is_you?(Map.get(map, "id"), you_id)} end) 
     #|> IO.inspect(label: "list_of_snake_structs")
-    
-  end
-  
-  def is_you?(id, you_id) do
-    id == you_id
-  end
-  
-  def convert_to_coords(list_of_coord_maps) do 
-    Enum.map(list_of_coord_maps, fn map -> [x: Map.get(map, "x"), y: Map.get(map, "y")] end) 
   end
   
    # BattleSnake API responses
@@ -82,14 +72,15 @@ defmodule SupaBattleSnake do
   
   
   def optimal_move(game_data, game_state) do 
-
     move = game_data
-    |> convert_game_data(game_state)
-    |> IO.inspect(label: "optimal_move converted game_data")
-    |> avoid_self()
-    |> avoid_wall()
-    |> avoid_opponents()
-    |> determine_move()
+      |> convert_game_data(game_state)
+      |> IO.inspect(label: "optimal_move converted game_data")
+      |> avoid_self()
+      |> avoid_wall()
+      |> avoid_opponents()
+      |> avoid_head_to_head()
+      |> choose_adjacent_food()
+      |> determine_strategic_move()
     
     MoveAgent.set_turn_move(game_data["turn"], %{
         "move" => move, 
@@ -97,33 +88,78 @@ defmodule SupaBattleSnake do
     })
   end
   
-  def determine_move(%GameBoard{up: up, right: right, down: down, left: left} = _game_data) do 
+  def determine_random_move(%GameBoard{up: up, right: right, down: down, left: left} = _game_data) do 
     %{up: up, right: right, down: down, left: left}
-    |> filter_map_by_value(0.0)
-    |> IO.inspect(label: "filtered options")
-    |> Map.keys()
-    |> Enum.take_random(1)
-    |> IO.inspect(label: "move")
-    |> List.first()
-    |> Atom.to_string()
+      |> filter_map_by_value(0.0)
+      |> filter_map_less_than_value(0.0)
+      |> IO.inspect(label: "filtered options")
+      |> Map.keys()
+      |> Enum.take_random(1)
+      |> IO.inspect(label: "move")
+      |> List.first()
+      |> Atom.to_string()
+  end
+  
+  def determine_strategic_move(%GameBoard{up: up, right: right, down: down, left: left} = game_data) do 
+    options = %{up: up, right: right, down: down, left: left}
     
+    choose_boost_or_random = boosted_option_exists?(options)
+    |> IO.inspect(label: "boosted_option_exists")
+    case choose_boost_or_random do
+      true -> options
+                |> filter_map_by_value(0.0)
+                |> filter_map_less_than_value(0.0)
+                |> IO.inspect(label: "filtered options")
+                |> Enum.max_by(fn {_k,v}-> v end)
+                |> Tuple.to_list()
+                |> List.first()
+                |> IO.inspect(label: "move")
+                |> Atom.to_string()
+      false -> determine_random_move(game_data)
+    end
+  end
+  
+  def boosted_option_exists?(options) do
+    !Enum.all?(options, fn {_k,v} -> v <= 0.5 end)
+  end
+  
+  def choose_adjacent_food(%GameBoard{board_food: board_food} = game_data) do
+    you_head = get_you(game_data, :head)
+    
+    game_data
+    |> update_beneficial_moves(:up, you_head, board_food)
+    |> update_beneficial_moves(:right, you_head, board_food)
+    |> update_beneficial_moves(:down, you_head, board_food)
+    |> update_beneficial_moves(:left, you_head, board_food)
+  end
+  
+  def update_beneficial_moves(game_data, move, head, check_coords) do
+    beneficial = check_coord_occupied?( future_coords(head, move), check_coords ) 
+    case beneficial do
+        true -> boost_option(game_data, move)
+        false -> game_data
+    end
+  end
+  
+  def boost_option(%GameBoard{up: up, right: right, down: down, left: left} = game_data, move) do
+      game_data
+      |> Map.put(move, Map.fetch!(game_data, move)+0.1)
   end
   
   def avoid_self(%GameBoard{} = game_data) do
-  
     you_head = get_you(game_data, :head)
-    you_body = get_you(game_data, :body)
+    body = get_you(game_data, :body)
+    
+    you_body = body -- [List.last(body)]
     
     game_data
     |> update_legal_moves(:up, you_head, you_body)
     |> update_legal_moves(:right, you_head, you_body)
     |> update_legal_moves(:down, you_head, you_body)
     |> update_legal_moves(:left, you_head, you_body)
-    
   end
   
   def avoid_wall(%GameBoard{board_height: board_height} = game_data) do
-    
     you_head = get_you(game_data, :head)
     
     game_data
@@ -131,11 +167,18 @@ defmodule SupaBattleSnake do
     |> update_legal_moves(:right, you_head, wall_coords(board_height, :right))
     |> update_legal_moves(:down, you_head, wall_coords(board_height, :bottom))
     |> update_legal_moves(:left, you_head, wall_coords(board_height, :left))
-    
+  end
+  
+  def wall_coords(board_height, location) do
+    case location do
+      :top -> Enum.map(-1..board_height, fn x -> %{"x" => x, "y" => 11 } end)
+      :right -> Enum.map(-1..board_height, fn y -> %{"x" => 11, "y" => y } end)
+      :bottom -> Enum.map(-1..board_height, fn x -> %{"x" => x, "y" => -1 } end)
+      :left -> Enum.map(-1..board_height, fn y -> %{"x" => -1, "y" => y } end) 
+    end
   end
   
   def avoid_opponents(%GameBoard{snakes: snakes} = game_data) do
-
     you_head = get_you(game_data, :head)
     
     combined_opponent_coords = Enum.map(snakes, fn snake -> snake.body end) |> List.flatten
@@ -145,37 +188,75 @@ defmodule SupaBattleSnake do
     |> update_legal_moves(:right, you_head, combined_opponent_coords)
     |> update_legal_moves(:down, you_head, combined_opponent_coords)
     |> update_legal_moves(:left, you_head, combined_opponent_coords)
+  end
+  
+  def avoid_head_to_head(%GameBoard{snakes: snakes} = game_data) do
+    you_head = get_you(game_data, :head)
     
+    combined_opponent_coords = Enum.map(snakes, fn snake -> snake.head end) |> List.flatten
+    
+    game_data
+    |> update_legal_moves(:up_ccw, you_head, combined_opponent_coords, :downgrade)
+    |> update_legal_moves(:up_up, you_head, combined_opponent_coords, :downgrade)
+    |> update_legal_moves(:up_cw, you_head, combined_opponent_coords, :downgrade)
+    |> update_legal_moves(:right_ccw, you_head, combined_opponent_coords, :downgrade)
+    |> update_legal_moves(:right_right, you_head, combined_opponent_coords, :downgrade)
+    |> update_legal_moves(:right_cw, you_head, combined_opponent_coords, :downgrade)
+    |> update_legal_moves(:down_ccw, you_head, combined_opponent_coords, :downgrade)
+    |> update_legal_moves(:down_down, you_head, combined_opponent_coords, :downgrade)
+    |> update_legal_moves(:down_cw, you_head, combined_opponent_coords, :downgrade)
+    |> update_legal_moves(:left_ccw, you_head, combined_opponent_coords, :downgrade)
+    |> update_legal_moves(:left_left, you_head, combined_opponent_coords, :downgrade)
+    |> update_legal_moves(:left_cw, you_head, combined_opponent_coords, :downgrade)
   end
   
-  def wall_coords(board_height, location) do
 
-    case location do
-      :top -> Enum.map(-1..board_height, fn x -> %{"x" => x, "y" => 11 } end)
-      :right -> Enum.map(-1..board_height, fn y -> %{"x" => 11, "y" => y } end)
-      :bottom -> Enum.map(-1..board_height, fn x -> %{"x" => x, "y" => -1 } end)
-      :left -> Enum.map(-1..board_height, fn y -> %{"x" => -1, "y" => y } end) 
-    end
+  def remove_option(game_data, move) do
+    Map.put(game_data, simplify_move(move), 0.0)
   end
   
-  def remove_option(game_data, move) do
-    Map.put(game_data, move, 0.0)
-  end
- 
   def update_option(game_data, move, update_value) do
-    Map.put(game_data, move, update_value)
+    Map.put(game_data, simplify_move(move), update_value)
   end 
   
   def update_legal_moves(game_data, move, head, check_coords) do
-  
     not_legal = check_coord_occupied?( future_coords(head, move), check_coords ) 
     case not_legal do
         true -> remove_option(game_data, move)
         false -> game_data
     end
-    
   end
   
+  def update_legal_moves(game_data, move, head, check_coords, :downgrade) do
+    discouraged = check_coord_occupied?( future_coords(head, move), check_coords ) 
+    simple_move = simplify_move(move)
+    case discouraged do
+        true -> update_option(game_data, simple_move, Map.fetch!(game_data, simple_move)-0.1)
+        false -> game_data
+    end
+  end
+  
+  def simplify_move(move) do 
+    case move do
+      :up_ccw-> :up
+      :up_up-> :up
+      :up_cw-> :up
+      
+      :right_ccw-> :right
+      :right_right-> :right
+      :right_cw-> :right
+      
+      :down_ccw-> :down
+      :down_down-> :down 
+      :down_cw-> :down
+      
+      :left_ccw-> :left
+      :left_left-> :left 
+      :left_cw-> :left
+      
+      _ -> move
+    end
+  end
   
   def future_coords(%{"x" => x_coord, "y" => y_coord} = _head, potential_option) do 
     case potential_option do 
@@ -183,13 +264,33 @@ defmodule SupaBattleSnake do
       :right -> %{"x" => x_coord+1, "y" => y_coord} |> IO.inspect(label: "future_coords(:right)")
       :down -> %{"x" => x_coord, "y" => y_coord-1} |> IO.inspect(label: "future_coords(:down)")
       :left -> %{"x" => x_coord-1, "y" => y_coord} |> IO.inspect(label: "future_coords(:left)")
+      
+      :up_ccw-> %{"x" => x_coord-1, "y" => y_coord+1} |> IO.inspect(label: "future_coords(:up_ccw)")
+      :up_up-> %{"x" => x_coord, "y" => y_coord+2} |> IO.inspect(label: "future_coords(:up_up)") 
+      :up_cw->%{"x" => x_coord+1, "y" => y_coord+1} |> IO.inspect(label: "future_coords(:up_cw)") 
+      
+      :right_ccw-> %{"x" => x_coord+1, "y" => y_coord+1} |> IO.inspect(label: "future_coords(:right_ccw)")
+      :right_right-> %{"x" => x_coord+2, "y" => y_coord} |> IO.inspect(label: "future_coords(:right_right)") 
+      :right_cw-> %{"x" => x_coord+1, "y" => y_coord-1} |> IO.inspect(label: "future_coords(:right_cw)") 
+      
+      :down_ccw-> %{"x" => x_coord+1, "y" => y_coord-1} |> IO.inspect(label: "future_coords(:down_ccw)")
+      :down_down-> %{"x" => x_coord, "y" => y_coord-2} |> IO.inspect(label: "future_coords(:down_down)") 
+      :down_cw-> %{"x" => x_coord-1, "y" => y_coord-1} |> IO.inspect(label: "future_coords(:down_cw)") 
+      
+      :left_ccw-> %{"x" => x_coord-1, "y" => y_coord-1} |> IO.inspect(label: "future_coords(:left_ccw)")
+      :left_left-> %{"x" => x_coord-2, "y" => y_coord} |> IO.inspect(label: "future_coords(:left_left)") 
+      :left_cw-> %{"x" => x_coord-1, "y" => y_coord+1} |> IO.inspect(label: "future_coords(:left_cw)") 
+      
+      :up_arc -> [%{"x" => x_coord-1, "y" => y_coord+1}, %{"x" => x_coord, "y" => y_coord+2}, %{"x" => x_coord+1, "y" => y_coord+1}] |> IO.inspect(label: "future_coords(:up_arc)")
+      :right_arc -> [%{"x" => x_coord+1, "y" => y_coord+1}, %{"x" => x_coord+2, "y" => y_coord}, %{"x" => x_coord+1, "y" => y_coord-1}] |> IO.inspect(label: "future_coords(:right_arc)")
+      :down_arc -> [%{"x" => x_coord+1, "y" => y_coord-1}, %{"x" => x_coord, "y" => y_coord-2}, %{"x" => x_coord-1, "y" => y_coord-1}] |> IO.inspect(label: "future_coords(:down_arc)")
+      :left_arc -> [%{"x" => x_coord-1, "y" => y_coord-1}, %{"x" => x_coord-2, "y" => y_coord}, %{"x" => x_coord-1, "y" => y_coord+1}] |> IO.inspect(label: "future_coords(:left_arc)")
     end
   end
   
   def check_coord_occupied?(%{"x" => _x_coord, "y" => _y_coord} = potential_coord, %{"x" => _x_coord_game, "y" => _y_coord_game} = game_coord) do
     potential_coord == game_coord
   end
-  
   
   def check_coord_occupied?(%{"x" => _x_coord, "y" => _y_coord} = potential_coord, game_coords) when is_list(game_coords) do
     find_result = Enum.find(game_coords, fn check_coord -> check_coord == potential_coord end)
@@ -203,20 +304,22 @@ defmodule SupaBattleSnake do
   
   
   def filter_map_by_value(map, value) do
-    #:maps.filter fn _, v -> v != value end, map
     map
     |> Enum.filter(fn {_k, v} -> v != value end)
     |> Enum.into(%{})
-    
   end
- 
+  
+  def filter_map_less_than_value(map, value) do
+    map
+    |> Enum.filter(fn {_k, v} -> v > value end)
+    |> Enum.into(%{})
+  end
+  
+  
   def get_you(%GameBoard{snakes: snakes, you_id: you_id}) do
     you = snakes
-    #|> IO.inspect(label: "all snakes")
-    |> Enum.filter(fn snake -> snake.id == you_id end)
-    #|> IO.inspect(label: "you snake")
-    |> List.first
-    you
+      |> Enum.filter(fn snake -> snake.id == you_id end)
+      |> List.first
   end
   
   def get_you(%GameBoard{snakes: snakes, you_id: you_id}, :head) do
@@ -228,47 +331,9 @@ defmodule SupaBattleSnake do
     you_snake = get_you(%GameBoard{snakes: snakes, you_id: you_id})
     you_snake.body
   end
-  
-
- 
-  def demo_snakes() do
-    [
-      %SupaBattleSnake.Snake{
-        body: [
-          %{"x" => 2, "y" => 2},
-          %{"x" => 2, "y" => 3},
-          %{"x" => 2, "y" => 4},
-          %{"x" => 3, "y" => 4},
-          %{"x" => 3, "y" => 3},
-          %{"x" => 4, "y" => 3},
-          %{"x" => 5, "y" => 3},
-          %{"x" => 6, "y" => 3}
-        ],
-        head: %{"x" => 2, "y" => 2},
-        health: 62,
-        id: "gs_rYbkTXbpSgMQvvfQfpCRhgYb",
-        length: 8,
-        name: "ignatius",
-        shout: "",
-        you: false
-      },
-      %SupaBattleSnake.Snake{
-        body: [
-          %{"x" => 4, "y" => 4},
-          %{"x" => 4, "y" => 5},
-          %{"x" => 4, "y" => 6},
-          %{"x" => 4, "y" => 7}
-        ],
-        head: %{"x" => 4, "y" => 4},
-        health: 94,
-        id: "gs_mYfHbCgBCPrwbVTcbtbv7yBW", 
-        length: 4,
-        name: "Alpha_Tester",
-        shout: "highly strategic not random move",
-        you: true
-      }
-    ]
-    end
-  
+    
+  def is_you?(id, you_id) do
+    id == you_id
+  end
   
 end
--- test/supa_battle_snake_test.exs
+++ test/supa_battle_snake_test.exs
@@ -5,4 +5,92 @@ defmodule SupaBattleSnakeTest do
   # test "greets the world" do
   #   assert SupaBattleSnake.hello() == :world
   # end
+  
+  setup do 
+    game_data = %SupaBattleSnake.GameBoard{
+      board_food: [%{"x" => 3, "y" => 10}],
+      board_height: 11,
+      down: 0.5,
+      game_id: "6689cda7-d7bf-43bc-8c47-b6109861ba2a",
+      left: 0.5,
+      right: 0.5,
+      route_state: :move,
+      snakes: [
+        %SupaBattleSnake.Snake{
+          body: [
+            %{"x" => 1, "y" => 7},
+            %{"x" => 1, "y" => 6},
+            %{"x" => 1, "y" => 5},
+            %{"x" => 1, "y" => 4},
+            %{"x" => 1, "y" => 3},
+            %{"x" => 1, "y" => 2},
+            %{"x" => 2, "y" => 2},
+            %{"x" => 2, "y" => 1},
+            %{"x" => 1, "y" => 1},
+            %{"x" => 0, "y" => 1},
+            %{"x" => 0, "y" => 2},
+            %{"x" => 0, "y" => 3},
+            %{"x" => 0, "y" => 4},
+            %{"x" => 0, "y" => 5},
+            %{"x" => 0, "y" => 6},
+            %{"x" => 0, "y" => 7},
+            %{"x" => 0, "y" => 8}
+          ],
+          head: %{"x" => 1, "y" => 7},
+          health: 95,
+          id: "gs_bfcBVmHxKtj6WhRSBDbJPvX7",
+          length: 17,
+          name: "NopeRope",
+          shout: "",
+          you: false
+        },
+        %SupaBattleSnake.Snake{
+          body: [
+            %{"x" => 6, "y" => 10},
+            %{"x" => 7, "y" => 10},
+            %{"x" => 8, "y" => 10},
+            %{"x" => 9, "y" => 10},
+            %{"x" => 10, "y" => 10},
+            %{"x" => 10, "y" => 10}
+          ],
+          head: %{"x" => 6, "y" => 10},
+          health: 100,
+          id: "gs_yYGTpgSXSyTGx74cqwGtmMw3",
+          length: 6,
+          name: "Snake 1a",
+          shout: "6% ready",
+          you: false
+        },
+        %SupaBattleSnake.Snake{
+          body: [
+            %{"x" => 5, "y" => 5},
+            %{"x" => 6, "y" => 5},
+            %{"x" => 6, "y" => 6},
+            %{"x" => 6, "y" => 7},
+            %{"x" => 7, "y" => 7},
+            %{"x" => 7, "y" => 6},
+            %{"x" => 7, "y" => 5},
+            %{"x" => 8, "y" => 5}
+          ],
+          head: %{"x" => 5, "y" => 5},
+          health: 93,
+          id: "gs_hDf3H7F9SgyfQjKFRYGkbJRH",
+          length: 8,
+          name: "Heuristic Harry",
+          shout: "moving",
+          you: true
+        }
+      ],
+      timeout: 500,
+      turn: 72,
+      up: 0.5,
+      you_id: "gs_hDf3H7F9SgyfQjKFRYGkbJRH"
+    }
+    
+    {:ok, game_data: game_data}
+  end
+  
+  test "Check coords: you head", context do
+    assert SupaBattleSnake.get_you(context[:game_data], :head) ==  %{"x" => 5, "y" => 5}
+  end
 end
